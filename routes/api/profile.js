28bb4197a77085657d1d245099e8aac8b4279660
Elvan Hidayat <elvanhd@gmail.com>
2023-05-03 22:33:44 +0700

    delete profile education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -280,4 +280,29 @@ router.put(
   }
 );
 
+// @route    DELETE api/profile/education/:edu_id
+// @desc     Delete education from profile
+// @access   Private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.edu_id);
+
+    if (removeIndex > -1) {
+      profile.education.splice(removeIndex, 1);
+    }
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
