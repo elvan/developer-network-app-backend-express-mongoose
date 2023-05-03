36fcbb2bf02b17c13acbf483d21ea53ba87ed798
Elvan Hidayat <elvanhd@gmail.com>
2023-05-03 12:46:18 +0700

    connecting to mongodb with mongoose

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const MONGODB_URL =
+  process.env.MONGODB_URL || 'mongodb://localhost:27017/developer-network-app';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
