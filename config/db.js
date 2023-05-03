1dd0138961fb26a7f1880ce69f5a19d0dc07d570
Elvan Hidayat <elvanhd@gmail.com>
2023-05-03 19:05:59 +0700

    user registration

--- config/db.js
+++ config/db.js
@@ -6,6 +6,7 @@ const MONGODB_URL =
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URL, {
+      useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json());
+
 app.get('/', (req, res) => res.send('Welcome'));
 
 // Define Routes
