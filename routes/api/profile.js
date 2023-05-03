8d48c01a265db71d28bd3824409022146ac9c470
Elvan Hidayat <elvanhd@gmail.com>
2023-05-03 20:36:03 +0700

    get current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,30 @@
 const express = require('express');
 
+const Profile = require('../../models/Profile');
+const auth = require('../../middleware/auth');
+
 const router = express.Router();
 
-// @route    GET api/profile
-// @desc     Test route
-// @access   Public
-router.get('/', (req, res) => res.send('Profile route'));
+// @route    GET api/profile/me
+// @desc     Get current users profile
+// @access   Private
+router.get('/me', auth, async (req, res) => {
+  console.log('req.user', req.user);
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
